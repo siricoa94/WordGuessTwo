f46e1fe2a2a13e5422fdef372b7e55f2fc65b4a1
Andrew Sirico <12siricoa94@gmail.com>
2019-09-18 23:17:45 -0400

    started adding arrays to game.js as well as setting up for what will be in my index.html

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+const words = ["Goku","Vegeta","Gohan","Bulma","Piccolo","Kami","Krillin"];
+const correctGuess = [];
+const incorrectGuess = [];
+const underscore = [];
+const remainingGuess = 10;
+const startScore = 0;
+
+// This will be where we grab the containers from our index.html in order to push data into them
+const blank = document.getElementById("blank");
+const correct = document.getElementById("correct");
+const incorrect = document.getElementById("incorrect");
+const guess = document.getElementById("guess");
+const score = document.getElementById("score");
+
+score.textContent = startScore;
+
+guess.textContent = remainingGuess;
+
+
